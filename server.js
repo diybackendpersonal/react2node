d82159cc8a9a7e7ddf653a65191028b743843c7d
Shalom Mathew <shalommathew@Shaloms-MacBook-Pro.local>
2024-03-25 22:30:40 +0100

    idpate

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use(cors)
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://shalom:YWV0VlJH7aERcKbO@cluster0.wbnky.mongodb.net/', {
