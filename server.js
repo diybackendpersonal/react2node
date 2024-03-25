81b5e8111a75f82174b7bacf2e31f34e25767747
Shalom Mathew <shalommathew@Shaloms-MacBook-Pro.local>
2024-03-25 21:11:54 +0100

    update

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://shalom:YWV0VlJH7aERcKbO@cluster0.wbnky.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+});
+
+// Create mongoose schemas and models
+const Product = mongoose.model('Product', {
+    name: String,
+    price: Number,
+    image: Number,
+    desc: Number,
+
+
+});
+
+const Order = mongoose.model('Order', {
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+    },
+    quantity: Number,
+    info: Object,
+
+});
+
+// Routes
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.post('/products', async (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        desc: req.body.desc,
+
+    });
+
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.get('/orders', async (req, res) => {
+    try {
+        const orders = await Order.find().populate('product');
+        res.json(orders);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.post('/orders', async (req, res) => {
+    const order = new Order({
+        product: req.body.productId,
+        quantity: req.body.quantity,
+        info: req.body.info,
+
+    });
+
+    try {
+        const newOrder = await order.save();
+        res.status(201).json(newOrder);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-const Button = ({name, isFilled}) =>{
-    
-return (
-    <button id={isFilled ? 'fill_btn' : 'outline_btn'}>{name}</button>
-)
-}
-
-export default Button
